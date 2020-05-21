167976134f3830b3dd030138ce374aa8831cf236
Uwe Thuemmel <uwe.thuemmel@uzh.ch>
2020-05-21 19:37:00 +0200

    moved latest_google_date outside of main function

--- R/proc_keyword.R
+++ R/proc_keyword.R
@@ -10,14 +10,6 @@ proc_keyword <- function(keyword = "Insolvenz", n_windows = 2) {
 
   # only process once a day
 
-  latest_google_date <- function(keyword) {
-    if (fs::file_exists(path_keyword(keyword, "sa"))) {
-      max(read_keyword(keyword, "sa")$time)
-    } else {
-      as.Date("1900-01-01")
-    }
-  }
-
 
   previous_google_date <- latest_google_date(keyword)
   message()
@@ -52,5 +44,13 @@ stop_if_no_data <- function(keyword) {
   files_indicator_raw <- grep(keyword, list.files(path_data_raw("indicator_raw")), value = TRUE, fixed = TRUE)
   if (length(files_indicator) == 0 & (length(files_indicator_raw) == 0)) {
     stop("No existing files found for keyword '", keyword, "' Have you run proc_keyword_init()?")
+  }
+}
+
+latest_google_date <- function(keyword) {
+  if (fs::file_exists(path_keyword(keyword, "sa"))) {
+    max(read_keyword(keyword, "sa")$time)
+  } else {
+    as.Date("1900-01-01")
   }
 }
