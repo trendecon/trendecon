3abbfbbced2fbbacd0c6374edeb43c29a41cfa55
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2020-08-16 00:11:16 +0300

    also added proc_trendecon_at.R for Austria

---
+++ R/proc_trendecon_at.R
@@ -0,0 +1,29 @@
+#' Calculate Trendecon Main Indices for Germany
+#'
+#' @export
+#'
+proc_trendecon_de <- function() {
+
+  kw_trendecon <- c(
+    "Wirtschaftskrise",
+    "Kurzarbeit",
+    "arbeitslos",
+    "Insolvenz"
+  )
+
+  proc_index(kw_trendecon, "AT", 'trendecon')
+
+  indices_in_production <- c(
+    "trendecon"
+  )
+
+  # vintage mode, copy to 'daily'
+  lapply(indices_in_production, function(e) fs::file_copy(path_keyword(e, "at", "sa"), path_daily(), overwrite = TRUE))
+
+  # from now, copy to data/ch
+  lapply(indices_in_production, function(e) fs::file_copy(path_keyword(e, "at", "sa"), path_data("ch"), overwrite = TRUE))
+
+  # FIXME: also store d, w, m, f in data/de
+}
+
+
