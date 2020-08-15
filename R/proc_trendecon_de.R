a4d5a7ccaa94e41570f63f34b260f040b52e2a86
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2020-08-15 22:11:38 +0300

    made proc_trendecon_de.R a copy of proc_trendecon_ch.R and made it work for 'trendecon' index

--- R/proc_trendecon_de.R
+++ R/proc_trendecon_de.R
@@ -1,8 +1,29 @@
 #' Calculate Trendecon Main Indices for Germany
 #'
 #' @export
+#'
 proc_trendecon_de <- function() {
-  keywords <- c("Wirtschaftskrise", "Kurzarbeit", "arbeitslos", "Insolvenz")
-  proc_index(keywords, "de", "desi")
+
+  kw_trendecon <- c(
+    "Wirtschaftskrise",
+    "Kurzarbeit",
+    "arbeitslos",
+    "Insolvenz"
+  )
+
+  proc_index(kw_trendecon, "DE", 'trendecon')
+
+  indices_in_production <- c(
+    "trendecon"
+  )
+
+  # vintage mode, copy to 'daily'
+  lapply(indices_in_production, function(e) fs::file_copy(path_keyword(e, "de", "sa"), path_daily(), overwrite = TRUE))
+
+  # from now, copy to data/ch
+  lapply(indices_in_production, function(e) fs::file_copy(path_keyword(e, "de", "sa"), path_data("ch"), overwrite = TRUE))
+
+  # FIXME: also store d, w, m, f in data/de
 }
+
 
